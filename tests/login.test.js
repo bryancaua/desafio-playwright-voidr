360eaed91583bd188045b765d46ba3ebb7ec94f1
Bryan Cauã <bryanncaua@gmail.com>
2025-06-11 20:47:22 -0300

    feat: adicionado testes de login válido e inválido

---
+++ tests/login.test.js
@@ -0,0 +1,32 @@
+// 1. Caso de teste, usuário válido, login efetuado
+
+const { test, expect }  = require('@playwright/test');
+
+test('Login com credenciais válidas', async ({page}) => {
+
+    await page.goto ('https://www.saucedemo.com/');
+
+    await page.fill('#user-name', 'standard_user'); // usuário válido
+    await page.fill('#password', 'secret_sauce'); // senha válida
+
+    await page.click('#login-button');
+
+    // irá verificar se foi redirecionado para a página de produtos
+    await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
+});
+
+
+// 2. Caso de teste, usuário inválido e senha errada, falha no login
+
+test.only('Login com credenciais inválidas', async ({page}) => {
+
+    await page.goto ('https://www.saucedemo.com/');
+
+    await page.fill('#user-name', 'usuario_invalido');
+    await page.fill('#password', 'senha_invalida');
+
+    await page.click('#login-button');
+
+    const errorMessage = page.locator('[data-test="error"]');
+    await expect(errorMessage).toBeVisible();
+})
