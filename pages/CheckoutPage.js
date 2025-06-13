150adc5eab57fa61fa5d749da07c76310fc99d3a
Bryan Cauã <bryanncaua@gmail.com>
2025-06-13 14:33:44 -0300

    feat: criada página CheckoutPage.js

---
+++ pages/CheckoutPage.js
@@ -0,0 +1,37 @@
+class CheckoutPage {
+    constructor(page) {
+        this.page = page;
+        this.firstNameInput = '[data-test="firstName"]';
+        this.lastNameInput = '[data-test="lastName"]';
+        this.postalCodeInput = '[data-test="postalCode"]';
+        this.continueButton = '[data-test="continue"]';
+        this.finishButton = '[data-test="finish"]';
+        this.sucessMessage = '.complete-header';
+        this.errorMessage = '[data-test="error"]';
+    }
+
+    async fillCheckoutInfo(first, last, zip) {
+        await this.page.fill(this.firstNameInput, first);
+        await this.page.fill(this.lastNameInput, last);
+        await this.page.fill(this.postalCodeInput, zip);
+    }
+
+    async continue(){
+        await this.page.click(this.continueButton);
+
+    }
+
+    async finish() {
+        await this.page.click(this.finishButton);
+    }
+
+    async getSucessMessage() {
+        return this.page.locator(this.sucessMessage);
+    }
+
+    async getErrorMessage() {
+        return this.page.locator(this.errorMessage);
+    }
+}
+
+module.exports = {CheckoutPage};
